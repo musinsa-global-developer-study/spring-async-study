17fc115860c17a7cb2ec42f553fff25ff1db29d6
하수종 <hsz3855@gmail.com>
2022-12-20 19:55:33 +0900

    init project

---
+++ src/main/java/com/example/springasyncstudy/SpringAsyncStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springasyncstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsyncStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAsyncStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springasyncstudy/SpringAsyncStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springasyncstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAsyncStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
