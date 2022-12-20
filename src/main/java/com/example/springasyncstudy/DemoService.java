1e0ba26e0cc56f3ce1bc07937a1991d4ca762a04
하수종 <hsz3855@gmail.com>
2022-12-20 20:56:14 +0900

    feat : ServiceExcutor 샘플 코드 작성

--- src/main/java/com/example/springasyncstudy/DemoService.java
+++ src/main/java/com/example/springasyncstudy/DemoService.java
@@ -1,5 +1,14 @@
 package com.example.springasyncstudy;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,8 +18,39 @@ public class DemoService {
     public String methodA() {
         String result = "";
 
-        result += methodB(1);
-        result += methodB(2);
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        Callable<String> callableTask1 = () -> {
+            return methodB(1);
+        };
+
+        Callable<String> callableTask2 = () -> {
+            return methodB(2);
+        };
+
+        List<Callable<String>> callableTasks = new ArrayList<>();
+        callableTasks.add(callableTask1);
+        callableTasks.add(callableTask2);
+
+        // 일단 되는지 보자....
+        try{
+            List<Future<String>> futures = executor.invokeAll(callableTasks);
+            futures.forEach(stringFuture -> {
+                try {
+                    System.out.println(stringFuture.get());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        executor.shutdown();
+
+
 
         return result;
     }
@@ -18,7 +58,7 @@ public class DemoService {
     private String methodB(int num){
 
         try {
-            Thread.sleep(1000L);
+            Thread.sleep(3000L);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
